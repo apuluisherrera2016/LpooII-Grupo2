27021c41dea25b5125ecd6a18afb0a4e1c244f98
LUIS_NOTEBOOK\luis <luisgherrera44@gmail.com>
2020-09-01 21:14:38 -0300

    login

---
+++ Vistas/login.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using 
+
+
+namespace Vistas
+{
+    /// <summary>
+    /// Interaction logic for login.xaml
+    /// </summary>
+    public partial class login : Window
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEntrar_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUsuario.Text == )
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Usuario usuario = new Usuario();
+            
+        }
+
+
+    }
+}
