c000f71edb61baa78875ca1f25d5464b6983870b
RENE <RENE@MRI-BANGHO-I7>
2020-09-03 20:15:22 -0300

    vistas y cambio de estilos

--- Vistas/Tickets.xaml.cs
+++ Vistas/Tickets.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using ClasesBase;
 
 namespace Vistas
 {
@@ -22,5 +23,6 @@ namespace Vistas
         {
             InitializeComponent();
         }
+        Ticket ticket = new Ticket();
     }
 }
