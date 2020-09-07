c9b86fcb68e9922348f7652fc59b39ce76accf0a
RENE <RENE@MRI-BANGHO-I7>
2020-09-07 19:00:12 -0300

    p1 asignacions de butacas

---
+++ Vistas/Asignacion de Butacas.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Vistas
+{
+	/// <summary>
+	/// Lógica de interacción para Asignacion_de_Butacas.xaml
+	/// </summary>
+	public partial class Asignacion_de_Butacas : Window
+	{
+		public Asignacion_de_Butacas()
+		{
+			InitializeComponent();
+		}
+	}
+}
