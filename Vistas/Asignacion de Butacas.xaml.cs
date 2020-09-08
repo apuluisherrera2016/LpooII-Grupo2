9da43a27ed19eabcc043cc087ea96f8b10e22c6c
RENE <RENE@MRI-BANGHO-I7>
2020-09-08 10:06:26 -0300

    cambios en el diseño

--- Vistas/Asignacion de Butacas.xaml.cs
+++ Vistas/Asignacion de Butacas.xaml.cs
@@ -22,5 +22,20 @@ namespace Vistas
 		{
 			InitializeComponent();
 		}
+		void OnClick1(object sender, RoutedEventArgs e)
+		{
+			btn1.Background = Brushes.LightBlue;
+		}
+
+		void OnClick2(object sender, RoutedEventArgs e)
+		{
+			btn2.Background = Brushes.Pink;
+		}
+
+		void OnClick3(object sender, RoutedEventArgs e)
+		{
+			btn1.Background = Brushes.Pink;
+			btn2.Background = Brushes.LightBlue;
+		}
 	}
 }
